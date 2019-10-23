16781aafa7f751041cacc97f4441bbd95ce0c30f
Jenna Gordon <jenna.gordon@libertymutual.com>
2019-10-23 19:22:29 -0400

    added validation on input and messaging

--- bin2dec/src/main/java/Bin2Dec.java
+++ bin2dec/src/main/java/Bin2Dec.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Bin2Dec {
 
+	private static final String ONE = "1";
+	private static final String ZERO = "0";
+
 	static Scanner scanner = new Scanner(System.in);
+	private static Boolean playGame = true;
 
 	public static void main(String[] args) {
 		translateBinary();
@@ -10,10 +14,23 @@ public class Bin2Dec {
 	}
 
 	private static void translateBinary() {
-		System.out.println("Enter number");
+		
+		while (playGame) {
+			System.out.println("Enter number");
 
-		String input = scanner.nextLine();
-		System.out.println(input);
+			String input = scanner.nextLine();
+			validateInput(input);
 
+		}
+
+	}
+
+	private static void validateInput(String input) {
+
+		if (!input.equals(ONE) && !input.equals(ZERO)) {
+			System.out.println("You didn't enter a valid number");
+			System.out.println("GoodBye :(");
+			playGame = false;
+		}
 	}
 }
