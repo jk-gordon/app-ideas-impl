419f747407f1891171645247f564252270b784c5
Jenna Gordon <jenna.gordon@libertymutual.com>
2019-10-23 19:00:32 -0400

    introduced private function to take in input and print input

--- bin2dec/src/main/java/Bin2Dec.java
+++ bin2dec/src/main/java/Bin2Dec.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Bin2Dec {
 
+	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		translateBinary();
+
+	}
+
+	private static void translateBinary() {
+		System.out.println("Enter number");
+
+		String input = scanner.nextLine();
+		System.out.println(input);
 
 	}
 }
