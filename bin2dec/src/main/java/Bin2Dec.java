39f5f1869d0aee6036296b91e4c0da3109a542c6
Jenna Gordon <jenna.gordon@libertymutual.com>
2019-11-06 19:47:14 -0500

    added functionality to calculate the decimal

--- bin2dec/src/main/java/Bin2Dec.java
+++ bin2dec/src/main/java/Bin2Dec.java
@@ -7,30 +7,49 @@ public class Bin2Dec {
 
 	static Scanner scanner = new Scanner(System.in);
 	private static Boolean playGame = true;
+	private static String input;
 
 	public static void main(String[] args) {
+		// validateInput();
 		translateBinary();
 
 	}
 
+//	private static void validateInput() {
+//
+//		System.out.println("Enter number");
+//		input = scanner.nextLine();
+//
+//		if (!input.equals(ONE) && input.equals(ZERO)) {
+//			System.out.println("You didn't enter a valid number. Only ONE and ZERO are expected, GoodBye!!");
+//		} else {
+//			translateBinary(input);
+//		}
+//
+//	}
+
 	private static void translateBinary() {
-		
-		while (playGame) {
-			System.out.println("Enter number");
+		System.out.println("Enter number");
+		input = scanner.nextLine();
 
-			String input = scanner.nextLine();
-			validateInput(input);
+		int length = input.length();
+		int result = 0;
+		int count = 0;
 
+
+		for (int i=length-1; i >= 0; i--) {
+
+			int value = Character.getNumericValue(input.charAt(count));
+
+			count++;
+
+			if (value == 1) {
+				result += Math.pow(2, i);
+
+			}
+			System.out.println(value);
+			System.out.println(result);
 		}
 
-	}
-
-	private static void validateInput(String input) {
-
-		if (!input.equals(ONE) && !input.equals(ZERO)) {
-			System.out.println("You didn't enter a valid number");
-			System.out.println("GoodBye :(");
-			playGame = false;
-		}
 	}
 }
